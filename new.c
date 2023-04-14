9806bc0012a4ebca88f57d7e3775ed19ff4f73a1
Abhinav Kumar Maurya <abhinavmaurya0604@gmail.com>
2023-04-14 16:50:05 +0530

    #nothing

--- new.c
+++ new.c
@@ -1,7 +1,7 @@
 // #include<stdio.h>
 // #define ARRAY_SIZE sizeof(arr[n])/sizeof(arr[0])
 
-// void find_zero(int arr[],int n)
+// int find_zero(int arr[],int n)
 // {
 //     for (int i=0; i <n; i++)
 //     {
@@ -50,49 +50,49 @@
 // }
 /*Maximum Sum of Sub-Array of a Array*/
 #include <stdio.h>
-// #define ARRAY_SIZE sizeof(arr[n])/sizeof(arr[0]);
+#define ARRAY_SIZE sizeof(arr[n])/sizeof(arr[0]);
 
-// int maxSubArraySum(int arr[], int n)
-// {
-//    int max_so_far = arr[0];
-//    int curr_max = arr[0];
+int maxSubArraySum(int arr[], int n)
+{
+   int max_so_far = arr[0];
+   int curr_max = arr[0];
 
-//    for (int i = 1; i < n; i++)
-//    {
-//         curr_max = (curr_max + arr[i] > arr[i]) ? curr_max + arr[i] : arr[i];
-//         max_so_far = (max_so_far > curr_max) ? max_so_far : curr_max;
-//    }
+   for (int i = 1; i < n; i++)
+   {
+        curr_max = (curr_max + arr[i] > arr[i]) ? curr_max + arr[i] : arr[i];
+        max_so_far = (max_so_far > curr_max) ? max_so_far : curr_max;
+   }
    
-//    return max_so_far;
-// }
+   return max_so_far;
+}
 
-// int main()
-// {
-//     int n;
-//     printf("Enter The Array Size\n");
-//     scanf("%d",&n);
-//     printf("Enter The Elements OF Array\n");
-//     int arr[n];
-//     for (int i = 0; i < n; i++)
-//     {
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("The Entered Array\n");
-//     for (int i = 0; i < n; i++)
-//     {
-//         printf("%d ",arr[i]);
-//     }    
-//     int max_sum = maxSubArraySum(arr, n);
-//     printf("\nMaximum contiguous sum is %d", max_sum);
-//     return 0;
-// }
 int main()
 {
-    char c;
-    for (int i = 0; i < 5; i++)
+    int n;
+    printf("Enter The Array Size\n");
+    scanf("%d",&n);
+    printf("Enter The Elements OF Array\n");
+    int arr[n];
+    for (int i = 0; i < n; i++)
     {
-        scanf(" %c",&c);
-        printf("%d\n ",c);
+        scanf("%d",&arr[i]);
     }
+    printf("The Entered Array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }    
+    int max_sum = maxSubArraySum(arr, n);
+    printf("\nMaximum contiguous sum is %d", max_sum);
     return 0;
 }
+// int main()
+// {
+//     char c;
+//     for (int i = 0; i < 5; i++)
+//     {
+//         scanf(" %c",&c);
+//         printf("%d\n ",c);
+//     }
+//     return 0;
+// }
